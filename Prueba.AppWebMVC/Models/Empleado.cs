9c0b7ae7cc88faaa5b11d6935d4618ce3ba8261f
Vasquez <vasquez0215@outlook.com>
2025-03-12 11:13:41 -0600

    Actualizacion.

--- Prueba.AppWebMVC/Models/Empleado.cs
+++ Prueba.AppWebMVC/Models/Empleado.cs
@@ -35,23 +35,23 @@ public partial class Empleado
 
     public int? PuestoTrabajoId { get; set; }
 
-    public virtual ICollection<AsignacionBono> AsignacionBonos { get; set; } = new List<AsignacionBono>();
+    public virtual ICollection<AsignacionBono>? AsignacionBonos { get; set; } = new List<AsignacionBono>();
 
-    public virtual ICollection<AsignacionDescuento> AsignacionDescuentos { get; set; } = new List<AsignacionDescuento>();
+    public virtual ICollection<AsignacionDescuento>? AsignacionDescuentos { get; set; } = new List<AsignacionDescuento>();
 
-    public virtual ICollection<ControlAsistencium> ControlAsistencia { get; set; } = new List<ControlAsistencium>();
+    public virtual ICollection<ControlAsistencium>? ControlAsistencia { get; set; } = new List<ControlAsistencium>();
 
-    public virtual ICollection<EmpleadoPlanilla> EmpleadoPlanillas { get; set; } = new List<EmpleadoPlanilla>();
+    public virtual ICollection<EmpleadoPlanilla>? EmpleadoPlanillas { get; set; } = new List<EmpleadoPlanilla>();
 
-    public virtual ICollection<Empleado> InverseJefeInmediato { get; set; } = new List<Empleado>();
+    public virtual ICollection<Empleado>? InverseJefeInmediato { get; set; } = new List<Empleado>();
 
     public virtual Empleado? JefeInmediato { get; set; }
 
-    public virtual ICollection<JefeInmediato> JefeInmediatos { get; set; } = new List<JefeInmediato>();
+    public virtual ICollection<JefeInmediato>? JefeInmediatos { get; set; } = new List<JefeInmediato>();
 
-    public virtual PuestoTrabajo PuestoTrabajo { get; set; } = null!;
+    public virtual PuestoTrabajo? PuestoTrabajo { get; set; } = null!;
 
-    public virtual TipodeHorario TipoDeHorario { get; set; } = null!;
+    public virtual TipodeHorario? TipoDeHorario { get; set; } = null!;
 
-    public virtual ICollection<Vacacion> Vacacions { get; set; } = new List<Vacacion>();
+    public virtual ICollection<Vacacion>? Vacacions { get; set; } = new List<Vacacion>();
 }
